b9c5584041cb5afb4a566f9b61def2782496843e
Fluna29 <fluna293107@gmail.com>
2025-01-14 19:45:34 +0100

    Servidor ejercicio 4.2 completado

--- Ejercicio_4.2/Servidor.java
+++ Ejercicio_4.2/Servidor.java
@@ -3,35 +3,46 @@ package ejercicio_2;
 import java.io.*;
 import java.net.*;
 
-public class Servidor {
+public class Servidor extends Thread {
+
+    //Contador de clientes conectados
+    private static int contadorClientes = 0;
+    private int idCliente;
+
+    //Socket del cliente y puerto a conectarse
+    Socket skCliente;
     static final int PUERTO = 1500;
 
-    public void iniciarServidor() {
-        try (ServerSocket servidor = new ServerSocket(PUERTO)) {
-            System.out.println("Servidor escuchando en el puerto " + PUERTO);
+    public Servidor (Socket scliente) {
+        skCliente = scliente;
+        idCliente = ++contadorClientes;
+    }
 
+    public static void main(String[] args) {
+        try {
+            ServerSocket skServidor = new ServerSocket(PUERTO);
+            System.out.println("Escuchando en el puerto " + PUERTO);
             while (true) {
-                Socket cliente = servidor.accept();
-                System.out.println("Cliente conectado desde " + cliente.getInetAddress());
-
-                manejarCliente(cliente);
+                Socket skCliente = skServidor.accept();
+                System.out.println("Cliente " + (contadorClientes + 1) + " conectado desde " + skCliente.getInetAddress());
+                new Servidor(skCliente).start();
             }
         } catch (IOException e) {
             System.out.println("Error en el servidor: " + e.getMessage());
         }
     }
 
-    private void manejarCliente(Socket cliente) {
-        try (cliente) {
-            DataInputStream flujo_entrada = new DataInputStream(cliente.getInputStream());
-            DataOutputStream flujo_salida = new DataOutputStream(cliente.getOutputStream());
+    public void run() {
+        try{
+            DataInputStream flujo_entrada = new DataInputStream(skCliente.getInputStream());
+            DataOutputStream flujo_salida = new DataOutputStream(skCliente.getOutputStream());
 
             // Recibir nombre del archivo solicitado
             String nombreArchivo = flujo_entrada.readUTF();
-            System.out.println("El cliente desea recibir el archivo: " + nombreArchivo);
+            System.out.println("El cliente " + idCliente + " desea recibir el archivo: " + nombreArchivo);
 
             // Ruta del archivo
-            String filePath = ".\\Ejercicio_3.2\\ficheros_prueba\\" + nombreArchivo;
+            String filePath = ".\\Ejercicio_4.2\\ficheros_prueba\\" + nombreArchivo;
             File archivo = new File(filePath);
 
             if (archivo.exists() && archivo.isFile()) {
@@ -43,18 +54,20 @@ public class Servidor {
                     }
                 }
                 flujo_salida.writeUTF("\nFIN"); // Marcador de fin de archivo
-                System.out.println("Archivo enviado correctamente.");
+                System.out.println("Archivo enviado correctamente al cliente: " + idCliente);
             } else {
                 flujo_salida.writeUTF("\nERROR: El archivo no existe.");
                 System.out.println("Archivo no encontrado.");
+            }
+
+            //Cierro la conexión con el cliente
+            synchronized (ejercicio_2.Servidor.class) {
+                skCliente.close();
+                System.out.println("Cliente " + idCliente + " desconectado");
             }
         } catch (IOException e) {
             System.out.println("Error al manejar al cliente: " + e.getMessage());
         }
     }
 
-    public static void main(String[] args) {
-        Servidor servidor = new Servidor();
-        servidor.iniciarServidor();
-    }
 }
