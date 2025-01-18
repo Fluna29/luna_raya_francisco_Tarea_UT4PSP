b89c71c69f32b247d4ec1083cf7eb834b7ea8371
Fluna29 <fluna293107@gmail.com>
2025-01-18 13:42:30 +0100

    Cliente ejercicio 4.3 completado Servidor ejercicio 4.3 completado
    Ejercicio 4.3 completado

--- Ejercicio_4.3/Cliente.java
+++ Ejercicio_4.3/Cliente.java
@@ -1,3 +1,87 @@
 package ejercicio_3;
+
+import java.io.*;
+import java.net.*;
+
 public class Cliente {
+    private static final String HOST = "localhost";
+    private static final int PUERTO = 1500;
+
+    public Cliente() {
+        try (Socket socket = new Socket(HOST, PUERTO)) {
+            System.out.println("Conectado al servidor en el puerto " + PUERTO);
+            try {
+                DataOutputStream flujo_salida = new DataOutputStream(socket.getOutputStream());
+                DataInputStream flujo_entrada = new DataInputStream(socket.getInputStream());
+
+                BufferedReader lectorUsuario = new BufferedReader(new InputStreamReader(System.in));
+
+                //Introducimos el usuario y la contraseña
+                boolean sesionIniciada = false;
+                while (!sesionIniciada) {
+                    //Recibo mensaje del servidor para introducir el usuario
+                    System.out.println(flujo_entrada.readUTF());
+                    String usuario = lectorUsuario.readLine();
+                    flujo_salida.writeUTF(usuario);
+
+                    //Recibo mensaje del servidor para introducir la contraseña
+                    System.out.println(flujo_entrada.readUTF());
+                    String password = lectorUsuario.readLine();
+                    flujo_salida.writeUTF(password);
+
+                    //Recibo mensaje del servidor para saber si la autenticación ha sido correcta
+                    String mensaje = flujo_entrada.readUTF();
+                    if (mensaje.equals("Autenticación correcta.")) {
+                        sesionIniciada = true;
+                    } else {
+                        System.out.println(mensaje);
+                    }
+                }
+
+                boolean salir = false;
+                while (!salir) {
+                    System.out.println(flujo_entrada.readUTF());
+                    String opcion = lectorUsuario.readLine();
+                    flujo_salida.writeUTF(opcion);
+                    switch (opcion) {
+                        case "ls" -> {
+                            System.out.println(flujo_entrada.readUTF()); // Mensaje inicial
+                            String respuesta;
+                            while (!(respuesta = flujo_entrada.readUTF()).equals("FIN_LISTA")) {
+                                System.out.println(respuesta);
+                            }
+                        }
+
+                        case "get" -> {
+                            System.out.println(flujo_entrada.readUTF()); // Solicitud del servidor
+                            String nombreArchivo = lectorUsuario.readLine();
+                            flujo_salida.writeUTF(nombreArchivo);
+
+                            String mensajeServidor;
+                            while (!(mensajeServidor = flujo_entrada.readUTF()).equals("FIN_ARCHIVO")) {
+                                System.out.println(mensajeServidor);
+                                if (mensajeServidor.startsWith("ERROR")) {
+                                    break; // Salir si hay un error
+                                }
+                            }
+                        }
+
+                        case "exit" -> {
+                            System.out.println("\nDesconectando del servidor...");
+                            salir = true;
+                        }
+                        default -> System.out.println("Comando no válido.");
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Error al usar comandos: " + e.getMessage());
+            }
+        } catch (IOException e) {
+            System.out.println("Error en el cliente: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        new Cliente();
+    }
 }
--- Ejercicio_4.3/Servidor.java
+++ Ejercicio_4.3/Servidor.java
@@ -1,3 +1,122 @@
 package ejercicio_3;
-public class Servidor {
+
+import java.io.*;
+import java.net.*;
+
+public class Servidor extends Thread {
+
+    //Contador de clientes conectados
+    private static int contadorClientes = 0;
+    private int idCliente;
+
+    //Socket del cliente y puerto a conectarse
+    Socket skCliente;
+    static final int PUERTO = 1500;
+
+    private int estado = 1;
+
+    public Servidor (Socket scliente) {
+        skCliente = scliente;
+        idCliente = ++contadorClientes;
+    }
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket skServidor = new ServerSocket(PUERTO);
+            System.out.println("Escuchando en el puerto " + PUERTO);
+            while (true) {
+                Socket skCliente = skServidor.accept();
+                System.out.println("Cliente " + (contadorClientes + 1) + " conectado desde " + skCliente.getInetAddress());
+                new ejercicio_3.Servidor(skCliente).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Error en el servidor: " + e.getMessage());
+        }
+    }
+
+    public void run() {
+        try{
+            DataInputStream flujo_entrada = new DataInputStream(skCliente.getInputStream());
+            DataOutputStream flujo_salida = new DataOutputStream(skCliente.getOutputStream());
+
+            String user = "admin";
+            String password = "pwd secreta";
+
+            boolean sesionIniciada = false;
+            while (!sesionIniciada){
+                // Autenticación
+                flujo_salida.writeUTF("Introduce tu usuario: ");
+                String usuarioCliente = flujo_entrada.readUTF();
+                flujo_salida.writeUTF("Introduce tu contraseña: ");
+                String passwordCliente = flujo_entrada.readUTF();
+
+                if (!usuarioCliente.equals(user) || !passwordCliente.equals(password)) {
+                    flujo_salida.writeUTF("ERROR: Usuario o contraseña incorrectos.");
+                    System.out.println("Cliente " + idCliente + " no autenticado.");
+                }else{
+                    flujo_salida.writeUTF("Autenticación correcta.");
+                    System.out.println("Cliente " + idCliente + " autenticado.");
+                    sesionIniciada = true;
+                }
+            }
+
+            do {
+                flujo_salida.writeUTF("\nIntroduce el comando (ls/get/exit): ");
+                String comando = flujo_entrada.readUTF();
+
+                switch (comando) {
+                    case "ls" -> {
+                        System.out.println("El cliente " + idCliente + " ha utilizado el comando ls.");
+                        flujo_salida.writeUTF("Listado de archivos en el directorio del servidor:");
+                        File directorio = new File(".\\Ejercicio_4.3\\");
+                        File[] archivos = directorio.listFiles();
+                        if (archivos != null) {
+                            for (File archivo : archivos) {
+                                flujo_salida.writeUTF(archivo.getName());
+                            }
+                        }
+                        flujo_salida.writeUTF("FIN_LISTA"); // Marcador para finalizar la lista de archivos
+                    }
+                    case "get" -> {
+                        System.out.println("El cliente " + idCliente + " ha utilizado el comando get.");
+                        flujo_salida.writeUTF("\nIntroduce el nombre del archivo que deseas recibir:");
+                        String nombreArchivo = flujo_entrada.readUTF();
+
+                        String filePath = ".\\Ejercicio_4.2\\ficheros_prueba\\" + nombreArchivo;
+                        File archivo = new File(filePath);
+
+                        if (archivo.exists() && archivo.isFile()) {
+                            flujo_salida.writeUTF("\nArchivo encontrado. Enviando contenido...\n");
+                            try (BufferedReader lector = new BufferedReader(new FileReader(archivo))) {
+                                String linea;
+                                while ((linea = lector.readLine()) != null) {
+                                    flujo_salida.writeUTF(linea);
+                                }
+                            }
+                            flujo_salida.writeUTF("FIN_ARCHIVO"); // Marcador de fin de archivo
+                            System.out.println("Archivo enviado correctamente al cliente: " + idCliente);
+                        } else {
+                            flujo_salida.writeUTF("ERROR: El archivo no existe.");
+                            System.out.println("Archivo no encontrado.");
+                        }
+                    }
+                    case "exit" -> {
+                        flujo_salida.writeUTF("\nDesconexión del servidor.");
+                        estado = -1; // Salir del bucle
+                    }
+                    default -> System.out.println("Comando no reconocido.");
+                }
+            } while (estado != -1);
+
+
+            //Cierro la conexión con el cliente
+            synchronized (ejercicio_3.Servidor.class) {
+                skCliente.close();
+                System.out.println("Cliente " + idCliente + " desconectado");
+            }
+        } catch (IOException e) {
+            System.out.println("Error al manejar al cliente: " + e.getMessage());
+        }
+    }
+
 }
